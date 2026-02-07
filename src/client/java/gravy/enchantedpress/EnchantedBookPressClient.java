f444d20f03e24d6f6342ff1dcd4ac8f3346964db
owenjones <ojones_6tc@outlook.com>
2026-02-07 15:12:34 +0000

    Initial Commit

---
+++ src/client/java/gravy/enchantedpress/EnchantedBookPressClient.java
@@ -0,0 +1,10 @@
+package gravy.enchantedpress;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnchantedBookPressClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
